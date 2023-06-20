5b950289c09154fc1c5e09baeae36e754fee72a4
shsep <iwtghome@gmail.com>
2023-06-20 15:55:53 +0900

    feat: 사용자 추가

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,11 +2,15 @@
   import Router from "svelte-spa-router"
   import Home from "./routes/Home.svelte"
   import Navigation from "./components/Navigation.svelte"
+  import UserCreate from "./routes/UserCreate.svelte"
+  import UserLogin from "./routes/UserLogin.svelte"
   import Detail from "./routes/Detail.svelte"
   import QuestionCreate from "./routes/QuestionCreate.svelte"
 
   const routes = {
     "/": Home,
+    "/user-create": UserCreate,
+    "/user-login": UserLogin,
     "/detail/:question_id": Detail,
     "/question-create": QuestionCreate,
   }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.user import user_router
 from domain.question import question_router
 from domain.answer import answer_router
 
@@ -19,5 +20,6 @@ app.add_middleware(
 )
 
 
+app.include_router(user_router.router)
 app.include_router(question_router.router)
 app.include_router(answer_router.router)
